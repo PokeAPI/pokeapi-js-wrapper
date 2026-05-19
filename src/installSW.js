bc655aeb07c1026c8c8f00799f87c0cc9e5890d5
Naramsim <igougi.ui@gmail.com>
2026-05-19 14:15:31 +0200

    refactor: use async await

--- src/installSW.js
+++ src/installSW.js
@@ -1,13 +1,12 @@
 import { log } from './utils.js'
 
-export function installSW(swLocation) {
+export async function installSW(swLocation) {
     if (navigator && window && 'serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register(`${swLocation}pokeapi-js-wrapper-sw.js`, { scope: './' })
-                .catch(error => {
-                    log('SW installation failed with the following error:')
-                    log(error)
-                })
-        })
+        try {
+            const r = await navigator.serviceWorker.register(`${swLocation}pokeapi-js-wrapper-sw.js`, { scope: './' })
+        } catch (error) {
+            log('SW installation failed with the following error:')
+            log(error)
+        }
     }
 }
